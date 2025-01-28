9656bc97b0886149382beedf577253d3a9e9a4ed
SandaminiObadage <hashiniobadage6030@gmail.com>
2025-01-28 22:20:06 +0530

    Add auth service to ecom-frontend

--- apps/ecom-frontend/src/app/app.config.ts
+++ apps/ecom-frontend/src/app/app.config.ts
@@ -5,11 +5,47 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { environments } from '../environments/environment';
+
+import { SsrStorageService } from './auth/ssr-storage.service';
+import { QueryClient } from '@tanstack/query-core';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { AbstractSecurityStorage, authInterceptor, LogLevel, provideAuth } from 'angular-auth-oidc-client';
+import { provideQueryClient } from '@tanstack/angular-query-experimental';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes,withComponentInputBinding()),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor()])),
+    provideAuth({
+      config: {
+        authority: environments.kinde.authority,
+        redirectUrl: environments.kinde.redirectUrl,
+        postLogoutRedirectUri: environments.kinde.postLogoutRedirectUri,
+        clientId: environments.kinde.clientId,
+        scope: 'openid profile email offline',
+        responseType: 'code',
+        silentRenew: true,
+        useRefreshToken: true,
+        logLevel: LogLevel.Warn,
+        secureRoutes: [environments.apiUrl],
+        customParamsAuthRequest: {
+          audience: environments.kinde.audience,
+        },
+      },
+    }),
+    { provide: AbstractSecurityStorage, useClass: SsrStorageService },
+    provideQueryClient(new QueryClient()),
+    provideNgxStripe(environments.stripePublishableKey),
   ],
 };
+function withComponentInputBinding(): import("@angular/router").RouterFeatures {
+  throw new Error('Function not implemented.');
+}
+
+function provideNgxStripe(stripePublishableKey: any): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
+  throw new Error('Function not implemented.');
+}
+
---
+++ apps/ecom-frontend/src/app/shared/model/user.model.ts
@@ -0,0 +1,32 @@
+export type OrderStatus = 'PENDING' | 'PAID';
+
+export interface OrderedItems {
+  name: string;
+  quantity: number;
+  price: number
+}
+
+export interface UserOrderDetail {
+  publicId: string;
+  status: OrderStatus;
+  orderedItems: OrderedItems[];
+}
+
+export interface AdminOrderDetail {
+  publicId: string;
+  status: OrderStatus;
+  email: string;
+  address: string;
+  orderedItems: OrderedItems[];
+}
+
+export interface ConnectedUser {
+
+    id: string;
+  
+    username: string;
+  
+    authorities: string[];
+  
+  }
+  
